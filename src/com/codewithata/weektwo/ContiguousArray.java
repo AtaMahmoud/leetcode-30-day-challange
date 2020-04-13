cbc6b8f49278159c44a9feade0c1157e74a7c598
Ata Mahmoud <ata.ata250@hotmail.com>
2020-04-13 19:36:11 +0200

    Day 13 solution fix

--- src/com/codewithata/weektwo/ContiguousArray.java
+++ src/com/codewithata/weektwo/ContiguousArray.java
@@ -5,23 +5,23 @@ import java.util.Map;
 
 public class ContiguousArray {
     public int findMaxLength(int[] nums) {
-        int max=Integer.MIN_VALUE;
-        Map<Integer,Integer> sum=new HashMap<>();
-        int num=0;
-        sum.put(0,-1);
+        int max = 0;
+        Map<Integer, Integer> sum = new HashMap<>();
+        int num = 0;
+        sum.put(0, -1);
 
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i]==0)
+            if (nums[i] == 0)
                 num--;
             else
                 num++;
 
-            if(sum.containsKey(num))
-                max=Math.max(max,i-sum.get(num));
+            if (sum.containsKey(num))
+                max = Math.max(max, i - sum.get(num));
             else
-                sum.put(num,1);
+                sum.put(num, i);
         }
 
-       return max;
+        return max;
     }
 }
