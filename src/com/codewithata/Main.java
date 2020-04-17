d682bf5256939ef60dd028192264cda68362433a
Ata Mahmoud <ata.ata250@hotmail.com>
2020-04-17 17:11:50 +0200

    Day 17 solution

--- src/com/codewithata/Main.java
+++ src/com/codewithata/Main.java
@@ -3,13 +3,13 @@ package com.codewithata;
 import com.codewithata.weekthree.ProductOfArrayExceptSelf;
 import com.codewithata.weekthree.ValidParenthesisString;
 
+import java.util.stream.IntStream;
+
 
 public class Main {
 
     public static void main(String[] args) {
         ValidParenthesisString validParenthesisString=new ValidParenthesisString();
         System.out.println(validParenthesisString.checkValidString("(*())(*)"));;
-
-
     }
 }
---
+++ src/com/codewithata/weekthree/NumberOfIsland.java
@@ -0,0 +1,27 @@
+package com.codewithata.weekthree;
+
+public class NumberOfIsland {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    doDFS(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void doDFS(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0')
+            return;
+
+        grid[i][j] = '0';
+        doDFS(grid, i + 1, j);
+        doDFS(grid, i - 1, j);
+        doDFS(grid, i, j - 1);
+        doDFS(grid, i, j + 1);
+    }
+}
